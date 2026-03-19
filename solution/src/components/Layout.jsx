5246af0a9ce21105a83d738c295b10aa9d2a3baf
Raven <jzhang2892@wisc.edu>
2026-03-18 23:20:43 -0500

    get the design of homepage

--- solution/src/components/Layout.jsx
+++ solution/src/components/Layout.jsx
@@ -1,13 +1,24 @@
 import { Link, Outlet } from 'react-router-dom'
+import { Navbar, Nav, Container } from "react-bootstrap";
 import '../App.css'
 
 export default function Layout() {
   return (
     <div>
-      <nav className="m-3">
-        <Link to="/" className="me-3">Home</Link>
-        <Link to="/about/">About</Link>
-      </nav>
+      <Navbar style={{ backgroundColor: "#0a1f44" }} variant="dark">
+        <Container>
+        {/* need an icon */}
+        <Navbar.Brand href="/">Florida Birding Explorer</Navbar.Brand>
+        </Container>
+        <Nav className="ms-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/spots">Spots</Nav.Link>
+        <Nav.Link href="/gallery">Gallery</Nav.Link>
+        <Nav.Link href="/community">Community</Nav.Link>
+        <Nav.Link href="/favorite">Favorite</Nav.Link>
+        </Nav>
+        
+      </Navbar>
 
       <main>
         <Outlet />
--- solution/src/pages/HomePage.jsx
+++ solution/src/pages/HomePage.jsx
@@ -1,18 +1,62 @@
 import { useState } from 'react'
 import '../App.css'
-import { Button, Card } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 export default function HomePage() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <div className="w-100 h-100 d-flex justify-content-center align-items-center">
-      <Card className='m-4 p-2'>
-        <Card.Body className='text-center'>
-          <h1>Hello World!</h1>
-          <Button onClick={() => setCount(o => o + 1)}>Count: {count}</Button>
-        </Card.Body>
-      </Card>
+//     --------------------------------------------------
+// Navigation Bar
+// Logo / Site Title                Home Spots Gallery Community Favorite
+// --------------------------------------------------
+
+// Hero Section
+// Big Title + Short Intro          Featured Bird Photo
+// Button / CTA
+
+// --------------------------------------------------
+
+// Featured Bird Section
+// Large Bird Image
+// Bird Name + Description
+
+// --------------------------------------------------
+
+// Why Florida is Great for Birdwatching
+// [Card] Diverse Habitats
+// [Card] Year-Round Birding
+// [Card] Rare and Colorful Birds
+
+// --------------------------------------------------
+
+// Explore the Website
+// [Spots] [Gallery] [Community] [Favorite]
+
+// --------------------------------------------------
+
+// Footer
+// --------------------------------------------------
+    <div style={{ backgroundColor: "#f4f8fc", padding: "60px 0" }}>
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <h1>Discover the Beauty of Birdwatching in Florida</h1>
+                        <p>
+                            Florida is one of the best birdwatching destinations...
+                        </p>
+                        <Button href="/spots">Explore Birding Spots</Button>
+                    </Col>
+
+                    <Col md={6}>
+                        {/* <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/8/82/Roseate_Spoonbill.jpg"
+                            style={{ width: "100%" }}
+                        /> */}
+                    </Col>
+                </Row>
+            </Container>
     </div>
   )
 }
